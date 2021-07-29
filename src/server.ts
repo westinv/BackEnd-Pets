fe67669d1d537c8279811592c3fecbe0c67f5cf6
westinv <43423127+westinv@users.noreply.github.com>
2021-07-29 20:12:23 -0300

    Documentação com o Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+
 import "./database";
 import "reflect-metadata";
-
 import { petsRoutes } from "./modules/Pets/routes/PetsRoutes";
 import { authenticateUserRoutes } from "./modules/Users/routes/AuthenticateUserRoutes";
 import { userRoutes } from "./modules/Users/routes/UsersRoutes";
+import swaggerFile from "./swagger.json";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(userRoutes);
 app.use(petsRoutes);
